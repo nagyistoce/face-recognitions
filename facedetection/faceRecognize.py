17558ae48ac809a8e90917a7911911910fbe9867
juliakjochum@gmail.com <juliakjochum@gmail.com@f236faba-5df7-439f-39a2-45de644ad76b>
2014-01-10 12:54:12 +0000

    Eye und mouth auskommentiert, graustuffenbild wird verkleinert

--- facedetection/faceRecognize.py
+++ facedetection/faceRecognize.py
@@ -243,25 +243,37 @@ class MyMainWindow(QtGui.QWidget):
 
         # ================================ Face Recognize =========================================== #
         aframe =frame.copy()
-
+        
         face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-        eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
-        mouth_cascade = cv2.CascadeClassifier("haarcascade_mcs_mouth.xml")
+        #eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
+        #mouth_cascade = cv2.CascadeClassifier("haarcascade_mcs_mouth.xml")
 
         img = aframe
+        #Image fuer Gesichts Erkennung vorbereiten
         g = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        faces = face_cascade.detectMultiScale(g, 1.3, 5)
+        detection_width = 320
+        scale = img.shape[1] / float(detection_width)
+        if img.shape[1] > detection_width:
+            scaled_height = int(img.shape[0]/scale +0.5)
+            smallg = cv2.resize(g, (detection_width,scaled_height))
+        else:
+            smallg = g
+        faces = face_cascade.detectMultiScale(smallg, 1.3, 5)
         for (x,y,w,h) in faces:
+            if img.shape[1] > detection_width:
+                x = int(x * scale + 0.5)
+                y = int(y * scale + 0.5)
+                w = int(w * scale + 0.5)
+                h = int(h * scale + 0.5)
             cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-            roi_gray = g[y:y+h, x:x+w]
+            #roi_gray = g[y:y+h, x:x+w]
 
-            roi_color = img[y:y+h, x:x+w]
-            eyes = eye_cascade.detectMultiScale(roi_gray)
-            for (ex,ey,ew,eh) in eyes:
-                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            #roi_color = img[y:y+h, x:x+w]
+            #eyes = eye_cascade.detectMultiScale(roi_gray)
+            #for (ex,ey,ew,eh) in eyes:
+                #cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
-            mouth = mouth_cascade.detectMultiScale(g, 1.3, 5)
+            #mouth = mouth_cascade.detectMultiScale(g, 1.3, 5)
             
         aframe= img
 
