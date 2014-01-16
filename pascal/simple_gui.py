ab9562dbe5aca8e4941415f427b8110a8fe41738
pascal.trebert@gmail.com <pascal.trebert@gmail.com@f236faba-5df7-439f-39a2-45de644ad76b>
2014-01-16 18:22:50 +0000

    OK Dummy Buttons OK Ende Button beendet per SIGNAL/SLOT OK Fenster schließen beendet per SIGNAL/SLOT

--- pascal/simple_gui.py
+++ pascal/simple_gui.py
@@ -1,33 +1,34 @@
+#-*- coding: utf-8 -*-
 '''
 Created on 16.01.2014
 
 @author: ptreb001
+
 '''
-from PyQt4 import QtCore
-from PyQt4 import Qt
-from PyQt4 import QtGui
-# from PyQt4 import QtOpenGL
 import sys
+
 import numpy as np
 import cv2
+
+from PyQt4 import Qt
+from PyQt4 import QtCore
+from PyQt4 import QtGui
 
 class Video():
     def __init__(self,capture):
         self.capture = capture
-        self.currentFrame=np.array([])
+        self.currentFrame=np.ndarray([])
  
     def captureNextFrame(self):
         """capture frame and reverse RBG BGR and return opencv image"""
         ret, readFrame=self.capture.read()
         if(ret==True):
-            print 'lese neues bild...'
             self.currentFrame=cv2.cvtColor(readFrame,cv2.COLOR_BGR2RGB)
  
     def convertFrame(self):
         """converts frame to format suitable for QtGui"""
         try:
             height,width=self.currentFrame.shape[:2]
-            print 'shape ', height, width
             img=QtGui.QImage(self.currentFrame,
                               width,
                               height,
@@ -40,24 +41,39 @@ class Video():
         
 class Gui(QtGui.QMainWindow):
     def __init__(self, *args):
+        """PyQt GUI fuer Button-Support und effiziente Kameraansteuerung."""
         QtGui.QWidget.__init__(self, *args)
-        
-        # selbst als MainWindow setzen
+        # selbst als Vater und Hauptwidget setzen 
         widget = QtGui.QWidget(self)
         self.setCentralWidget(widget)
-        
-        #Layout
+        # vertikales Layout setzen
         boxLayout = QtGui.QVBoxLayout()    
         widget.setLayout(boxLayout)
-        
         # QLabel als Videoframe Container
-        self.label = QtGui.QLabel("Ein QLabel")
-        boxLayout.addWidget(self.label)
+        self.video_label = QtGui.QLabel("Videobild")
+        boxLayout.addWidget(self.video_label)
 
+        # Foto-Button
+        self.foto_button = QtGui.QPushButton("Foto", self)
+        boxLayout.addWidget(self.foto_button)
+        # Wer-Bin-Ich-Button
+        self.who_am_i_button = QtGui.QPushButton("Wer-Bin-Ich?", self)
+        boxLayout.addWidget(self.who_am_i_button)
+        # Beenden-Button        
+        self.quit_button = QtGui.QPushButton("Ende", self)
+        boxLayout.addWidget(self.quit_button)
+        palette = QtGui.QPalette()
+        palette.setColor(self.quit_button.foregroundRole(),Qt.QColor("red"))
+        self.quit_button.setPalette(palette)
+        Qt.QObject.connect(self.quit_button,
+                           Qt.SIGNAL('clicked()'),
+                           Qt.qApp,
+                           Qt.SLOT('quit()')
+                           )
+        boxLayout.addWidget(self.quit_button)
         
-        # Video-Instanz um Videobild zu bearbeiten
+        # Video-Bild umwandeln und updaten
         self.video = Video(cv2.VideoCapture(0))
-        print type(self.video)
         self._timer = QtCore.QTimer(self)
         self._timer.timeout.connect(self.play)
         self._timer.start(27)
@@ -66,38 +82,22 @@ class Gui(QtGui.QMainWindow):
     def play(self):
         try:
             self.video.captureNextFrame()
-            self.label.setPixmap(self.video.convertFrame())
-            
-            # Bild in Qlabel anzeigen
-#             self.ui.videoFrame.setPixmap(
-#                 self.video.convertFrame())
-#             self.ui.videoFrame.setScaledContents(True)
+            self.video_label.setPixmap(self.video.convertFrame())
         except TypeError:
             print "No frame"
  
 def main(args):
+    """Hauptfenster, Hauptanwendung Initialisierung und Schliessen Signal anbinden""" 
     app = QtGui.QApplication(args)
-    ex = Gui()
-    ex.show()
+    win = Gui()
+    win.show()
+    app.connect(app,                             # Sender-Widget
+                Qt.SIGNAL('lastWindowClosed()'), # Signal
+                app,                             # Empfaenger
+                Qt.SLOT('quit()')                # aktivierter Slot
+            )
     return app.exec_()
     
-    
-class ToggleWindow(QtGui.QMainWindow):
-    def __init__(self, *args):
-        QtGui.QMainWindow.__init__(self, *args)
-        self.setCentralWidget(self)
-        self.adjustSize()
-        
-def main2(args):
-    app = QtGui.QApplication(args)
-    widget = QtGui.QWidget()    
-    boxLayout = QtGui.QVBoxLayout()    
-    widget.setLayout(boxLayout)
-    label = QtGui.QLabel("Nach Ein QLabel")
-    boxLayout.addWidget(label)
-    widget.show()
-    return app.exec_()
-
 if __name__ == '__main__':
-    # huebsch gemacht endlosschleifen aufruf app.exec_ als returnwert
+    # Endlosschleifen aufruf app.exec_ als returnwert
     sys.exit(main(sys.argv))
