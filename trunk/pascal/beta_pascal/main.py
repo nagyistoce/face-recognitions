5d06cdb1e6f7dfe31d3eccb9279027ca994b9ac7
pascal.trebert@gmail.com <pascal.trebert@gmail.com@f236faba-5df7-439f-39a2-45de644ad76b>
2014-01-17 13:36:50 +0000

    Meine Testumgebung

---
+++ trunk/pascal/beta_pascal/main.py
@@ -0,0 +1,34 @@
+import fdetection as fd
+import cv2
+
+class WebcamController(object):
+    #Webcam Zugriff
+    def __init__(self):
+        self.webcam = cv2.VideoCapture(0)
+    #oeffnet Webcam Fenster, stoesst Gesichtserkennung an
+    def startWebcam(self):
+        cv2.namedWindow("preview")
+        if self.webcam.isOpened(): # try to get the first frame
+            self.test, self.frame = self.webcam.read()
+            self.detect = fd.FaceDetector()
+        else:
+            self.test = False
+        
+        while self.test:
+            img = self.detect.detectFace(self.frame)
+            cv2.imshow("preview", img)
+
+            # get next frame
+            self.test, self.frame = self.webcam.read()
+
+            key = cv2.waitKey(20)
+            if key in [27, ord('Q'), ord('q')]: # exit on ESC
+                break
+        self.webcam.release()
+        cv2.destroyWindow("preview")
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        
+if __name__ == "__main__":
+    start = WebcamController()
+    start.startWebcam()
