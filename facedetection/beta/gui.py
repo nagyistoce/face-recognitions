dce4d06a9da2c2f87495791a4cbcb595bfe7a549
Deniz.Yoshimito <Deniz.Yoshimito@f236faba-5df7-439f-39a2-45de644ad76b>
2014-01-21 17:23:23 +0000

    OK: Buttons mit Funktionen

--- facedetection/beta/gui.py
+++ facedetection/beta/gui.py
@@ -15,7 +15,7 @@ import cv2
 from PyQt4 import Qt
 from PyQt4 import QtCore
 from PyQt4 import QtGui
-from ctypes.wintypes import DOUBLE
+
 
 class Video():
     """Klasse zum konvertieren des Videobilds"""
