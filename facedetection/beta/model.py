e0666828703723ce10a54b4454709213e48f2f9b
pascal.trebert@gmail.com <pascal.trebert@gmail.com@f236faba-5df7-439f-39a2-45de644ad76b>
2014-01-24 18:32:28 +0000

    OK: Bilderserie wird nach Buttonklick automatisch abgespeichert! YEAYYY

--- facedetection/beta/model.py
+++ facedetection/beta/model.py
@@ -7,31 +7,30 @@ import os
 import errno
 
 from PyQt4 import QtGui
+import cv2
 
-class TrainingSet(object):
+class TrainingSets(object):
     """Ein Trainings-Set d.h. eine Person mit ihren Gesichtern und ID."""
     
-    def __init__(self, id, path='~/Dropbox/FACERECOGNITION/_TRAINING_SETS_', name=''):      
-        self.ID = id
-        self.path = os.path.expanduser(os.path.join(path,str(id)))
+    def __init__(self, path='~/Dropbox/FACERECOGNITION/_TRAINING_SETS_', name=''):
+        self.path = os.path.expanduser(path)
         self.counter = 0
         self.name = name
         self.images = {}
-        print ' path ', self.path
+        print ' path den ich bekomme ', self.path
         # TOD0: automatisch Ordnerstruktur anlegen falls sie noch nicht existiert
         self.init_folder_structure()
     
-    def get_image_name(self):
+    def get_image_name(self, id):
         """Gibt den Bildnamen fuer ein neu zu speicherndes Gesicht zurueck"""
-        return '%s_%s.jpg' % (self.ID, self.counter)
+        return '%s_%s.jpg' % (str(id), self.counter)
     
-    def create_folder(self, path, name=''):
+    def create_folder(self, path, face_id=''):
         """Legt einen neuen Ordner im Dateisystem an: path/name."""
         # check ob Ordner bereits existiert
-        path = os.path.join(path, name)
-        print path
+        path = os.path.join(path, str(face_id))
         if not os.path.exists(path):
-            print 'path ', 'wird angelegt...'
+            print path , 'wird angelegt...'
             try:
                 os.makedirs(path)
             except OSError, e:
@@ -39,22 +38,25 @@ class TrainingSet(object):
                     print 'ignoriere os.error'
         else:
             print path, ' bereits vorhannden'        
-        
+    
+    # todo raus
     def init_folder_structure(self):
         """Legt Ordnerstrukur an"""
         self.create_folder(self.path)
 
-    def add_face(self, face):                
-        """Fuegt ein Gesichtsbild dem entsprechenden Ordner (self.ID) hinzu"""
-        folder = os.path.join(self.path)
-        print folder, ' folder checken'
+    def add_face(self, face, id):                
+        """Fuegt ein Gesichtsbild dem entsprechenden Ordner (self.ID) hinzu"""     
+        folder = os.path.join(self.path, str(id))
         
-        if os.path.exists(folder):
-            print 'Okay, ID existiert'
-            print type(face)
-            assert(isinstance(face, QtGui.QPixmap))
-            face.save(os.path.join(self.path, self.get_image_name()))
-            self.counter += 1
-        else:
-            print 'Fehler '
+        if not os.path.exists(folder):
+            self.create_folder(self.path, id)
+        print 'Okay, ID existiert'
+        cv2.imwrite(os.path.join(folder,self.get_image_name(id)), face)
+        #assert(isinstance(face, QtGui.QPixmap))
+        #face.save(os.path.join(self.path, self.get_image_name()))
+        self.counter += 1
+        
+            
+            
+            
         
