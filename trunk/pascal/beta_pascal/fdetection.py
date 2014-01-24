7d7f3dcc07b13aa6f25c5366187679026b2dec2d
pascal.trebert@gmail.com <pascal.trebert@gmail.com@f236faba-5df7-439f-39a2-45de644ad76b>
2014-01-24 14:52:42 +0000

    OK: Fotos werden korrekt in Unterordnern per KLICK angelegt

--- trunk/pascal/beta_pascal/fdetection.py
+++ trunk/pascal/beta_pascal/fdetection.py
@@ -10,42 +10,53 @@ from PyQt4 import QtGui
 class TrainingSet(object):
     """Ein Trainings-Set d.h. eine Person mit ihren Gesichtern und ID."""
     
-    def __init__(self, face_id, path='~/Dropbox/FACERECOGNITION/_TRAINING_SETS_', name=''):      
-        self.FACE_ID = face_id
-        self.path = os.path.expanduser(path)
+    def __init__(self, id, path='~/Dropbox/FACERECOGNITION/_TRAINING_SETS_', name=''):      
+        self.ID = id
+        self.path = os.path.expanduser(os.path.join(path,str(id)))
         self.counter = 0
         self.name = name
         self.images = {}
+        print ' path ', self.path
         # TOD0: automatisch Ordnerstruktur anlegen falls sie noch nicht existiert
-        
+        self.init_folder_structure()
+    
+    def get_image_name(self):
+        """Gibt den Bildnamen fuer ein neu zu speicherndes Gesicht zurueck"""
+        return '%s_%s.jpg' % (self.ID, self.counter)
+    
     def create_folder(self, path, name=''):
-        """Ueberprueft ob Ordner bereits existiert, und legt ihn ansonsten an"""
+        """Legt einen neuen Ordner im Dateisystem an: path/name."""
+        # check ob Ordner bereits existiert
         path = os.path.join(path, name)
         print path
         if not os.path.exists(path):
-            print path, ' wird angelegt...'
+            print 'path ', 'wird angelegt...'
             try:
                 os.makedirs(path)
             except OSError, e:
                 if e.errno == errno.EEXIST:
-                    # TODO: print rausnehmen 
                     print 'ignoriere os.error'
         else:
             print path, ' bereits vorhannden'        
         
     def init_folder_structure(self):
-        """ und legt diese bei Bedarf neu an"""
+        """Legt Ordnerstrukur an"""
         self.create_folder(self.path)
 
-    def add_frame(self, frame):                
-        """Fuegt ein Gesichtsbild dem entsprechenden Ordner (self.FACE_ID) hinzu"""
-        if os.path.exists(os.path.join(self.path, str(self.FACE_ID))):
-            print 'Okay, FACE_ID existiert'
-            print type(frame)
-            assert(isinstance(frame, QtGui.QPixmap))
-            frame.save(os.path.join(self.path, str(self.FACE_ID)) + '/bild.jpg')
+    def add_face(self, face):                
+        """Fuegt ein Gesichtsbild dem entsprechenden Ordner (self.ID) hinzu"""
+        folder = os.path.join(self.path)
+        print folder, ' folder checken'
+        
+        if os.path.exists(folder):
+            print 'Okay, ID existiert'
+            print type(face)
+            assert(isinstance(face, QtGui.QPixmap))
+            face.save(os.path.join(self.path, self.get_image_name()))
+            self.counter += 1
         else:
             print 'Fehler '
+        
 
 class Haarcascades(object):    
     """Stellt die Haarcascade-XML Dateien bereit."""
