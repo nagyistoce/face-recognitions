40468024e4eb618f77079ae79f67ddc1e3d4ab9b
pascal.trebert@gmail.com <pascal.trebert@gmail.com@f236faba-5df7-439f-39a2-45de644ad76b>
2014-01-29 05:11:29 +0000

    OK: get_faces() und get_all_faces() getestet -> OK. Bilder werden von Platte eingelesen TODO: vergleichen ob die Bilder tatsächlich identisch sind nach dem einlesen von Platte, erster Versuch mit MD5 Hashes mislungen Aufgeraeumt: ein unnötiger code entfernt, sinnvollere namen

--- trunk/facedetection/beta/model.py
+++ trunk/facedetection/beta/model.py
@@ -1,25 +1,29 @@
 '''
-Verwaltung der Persistenten Daten und Programmzustaende.
+Created on 24.01.2014
 
+@author: ptreb001
 '''
 import os
 import errno
 import sys
 
 import cv2, numpy as np
-import hashlib
-import binascii 
 
 class TrainingSets(object):
-    """Verwaltung der TrainingSets (vorab gespeicherte Gesichtsbilder) der Personen und ihrer IDs."""
+    """Ein Trainings-Set d.h. eine Person mit ihren Gesichtern und ID."""
     
-    def __init__(self, path='~/Dropbox/FACERECOGNITION/_TRAINING_SETS_'):
+    def __init__(self, path='~/Dropbox/FACERECOGNITION/_TRAINING_SETS_', name=''):
         self.path = os.path.expanduser(path)
         self.counter = 0
+        self.name = name
         self.images = {}
         print ' path den ich bekomme ', self.path
         # TOD0: automatisch Ordnerstruktur anlegen falls sie noch nicht existiert
         self.init_folder_structure()
+    
+    def get_image_name(self, face_id):
+        """Gibt den Bildnamen fuer ein neu zu speicherndes Gesicht zurueck"""
+        return '%s_%s.jpg' % (str(face_id), self.counter)
     
     def create_folder(self, path, face_id=''):
         """Legt einen neuen Ordner im Dateisystem an: path/name."""
@@ -44,47 +48,59 @@ class TrainingSets(object):
         """Fuegt ein Gesichtsbild dem entsprechenden Ordner (self.ID) hinzu"""     
         assert(isinstance(face, np.ndarray))
         folder = os.path.join(self.path, str(face_id))
+        
         if not os.path.exists(folder):
             self.create_folder(self.path, face_id)
-        image_name = self.get_image_name(face_id, face)
-        cv2.imwrite(os.path.join(folder, image_name), face)
+        cv2.imwrite(os.path.join(folder,self.get_image_name(face_id)), face)
         self.counter += 1
-        
-    def get_hash(self, string):
-        """Gibt md5 Hashwert des Objekts als String zurueck"""
-        m = hashlib.md5()
-        m.update(string)
-        digest = m.hexdigest()
-        return binascii.hexlify(digest)
-    
-    def get_image_name(self, face_id, face):
-        """Gibt den Bildnamen fuer ein neu zu speicherndes Gesicht zurueck"""
-        assert(isinstance(face, np.ndarray))
-        name = '%s_%s.jpg' % (str(face_id), self.get_hash(face.tostring())) #self.counter)
-        return name
 
-
-    # warum nicht jeder face_id ne liste von face_images zuordnen?
-    def get_faces(self, path):
+    def get_faces(self, id_path):
+        """Liest alle Bilder einer bestimmten ID ein"""
+        id_path = os.path.join(self.path, id_path)
+        face_images = []
+        for img in os.listdir(id_path):
+            try:
+                img_path = os.path.join(id_path, img)
+                im = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+                face_images.append(np.asarray(im, dtype = np.uint8))
+            except IOError,(errno,strerror):
+                print "I/O error{0}: {1}".format(errno, strerror)
+            except:
+                print "Unexpected error: ", sys.exc_info()[0]
+                raise
+        print 'Ich habe %s Bilder eingelesen' % len(face_images)
+        return face_images, len(face_images)
+            
+    def get_all_faces(self):
         """Einlesen der Gesichtsbilder von Platte mit zuordnung der jeweiligen ID durch 2 Listen."""
-        ids = 0
         face_images, face_ids = [], []
-        for dirname, dirnames, filenames in os.walk(path):
+        for dirname, dirnames, filenames in os.walk(self.path):
             for subdirname in dirnames:
-                id_path = os.path.join(dirname, subdirname)
-                for face_image in os.listdir(id_path):
-                    try:
-                        im = cv2.imread(os.path.join(id_path, face_image), cv2.IMREAD_GRAYSCALE)
-                        face_images.append(np.asarray(im, dtype = np.uint8))
-                        face_ids.append(ids)
-                    except IOError,(errno,strerror):
-                        print "I/O error{0}: {1}".format(errno, strerror)
-                    except:
-                        print "Unexpected error: ", sys.exc_info()[0]
-                        raise
-                ids = ids +1 # was passiert wenn auf platte nur id (0, 2) vorhanden sind
+                if subdirname != 'ID':
+                    id_path = os.path.join(dirname, subdirname)
+                    images, number = self.get_faces(id_path)
+                    face_images.extend(images)
+                    face_ids.extend([int(subdirname)] * number)
         return [face_images, face_ids]
     
+#     def get_faces(self):
+#         ids = 0
+#         face_images,face_ids = [],[]
+#         for dirname, dirnames,filenames in os.walk(self.path):
+#             for subdirname in dirnames:
+#                 id_path = os.path.join(dirname, subdirname)
+#                 for face_image in os.listdir(id_path):
+#                     try:
+#                         im = cv2.imread(os.path.join(id_path,face_image), cv2.IMREAD_GRAYSCALE)
+#                         face_images.append(np.asarray(im,dtype = np.uint8))
+#                         face_ids.append(ids)
+#                     except IOError,(errno,strerror):
+#                         print "I/O error{0}: {1}".format(errno,strerror)
+#                     except:
+#                         print "Unexpected error:", sys.exc_info()[0]
+#                         raise
+#                 ids = ids +1
+#         return [face_images,face_ids]
             
             
         
