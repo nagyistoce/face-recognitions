81889e0679cfb96e492ac823523c86e4d6a65a4a
pascal.trebert <pascal.trebert@f236faba-5df7-439f-39a2-45de644ad76b>
2014-02-05 07:08:37 +0000

    Observer: Observer-Pattern um die ermittelte ID/Name in der GUI anzuzeigen. FIXME: ID ermitteln optimieren

---
+++ pascal/beta/controller.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+Modul um GUI Eingaben korrekt zu verarbeiten und entsprechende Prozeduren anzustossen.
+
+"""
+import logging as log
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle    
+from fdetection import FaceDetector as fd
+import frecognize as fr
+import database as db
+
+class Controller(object):
+    """Steuert Facedetector und Facerecognizer Objekte je nach Eingaben in der GUI.
+    Das Dictionary id_dict hat als Schluessel die ID und zugehoerige Informationen als Liste in den Values
+    
+    id_dict {'id':['username', counter_predicted], ... }
+    values-Liste enthaelt: 
+    - Benutzername
+    - Counter wie oft diese ID predicted wurde im aktuellen Suchvorgang
+    
+    """
+
+    def __init__(self):
+        """Instanziiert immer ein Facedetector- und ein FaceRecognizer-Objekt."""
+        # Facedetekor-Objekt
+        self.detect = fd()
+        self.fr = fr.FaceRecognizer()
+        self.t_sets = db.TrainingSets()
+        self.trigger_rec = False
+        self.trigger_save = False
+        # dictionary d{'id':[#_imgs, [predict, predict, ...], 'username', ...]}
+        # ids enthaelt Informationen zu den IDs: Anzahl eingelesener Bilder, liste mit allen Predicts bei facedetection-Vorgang
+        self.id_dict = self.t_sets.get_id_dict()
+        self.observer = []
+        self.final_predict = None
+        
+    def get_final_predict(self):
+        """Gibt ID der Erkannten Person zurueck"""
+        return self.final_predict
+        
+    def register_observer(self, obj):
+        """Wird zum registrieren eines Observers verwendet"""
+        self.observer.append(obj)
+    def notify_observer(self):
+        """Ruft update() aller zu benachritigen Objekte auf"""
+        for obj in self.observer:
+            assert(obj.update)
+            obj.update()
+    
+    def get_percentage(self, total, part):
+        """Berrechnung des Prozentualen Anteils von part an total."""
+        try:
+            percent = 100 * part/float(total)
+        except ZeroDivisionError, e:
+            log.debug('Durch Null geteilt. Wenn kein Gesicht erkannt wurde kann es passieren. total = %s', total)
+        except:
+            log.exception('Unerwarteter Fehler beim Berrechnen des Prozentanteils.')
+        return percent
+    
+    def print_stat(self):
+        """Erkennungs-Statistik-Ausgabe auf Konsole"""
+        total = sum([n[1] for n in self.id_dict.values()])
+        s = ['\n----------------------------------------------']
+        for k, v in sorted(self.id_dict.items()):  
+            count = v[1]
+            #percentage = 100 * count/float(total)
+            s.append('Predicts: ID: %s    %sx => %s%%' % (k, count, self.get_percentage(total, count)))
+        s.append('total: %s' %total)
+        s.append('----------------------------------------------\n')
+        log.info('\n'.join(s))
+        
+    def frame_to_face(self, frame, face_id, save_face, recognize_face):
+        """Verarbeitet pro Frame die Informationen der gedrueckten Buttons und gibt bearbeiteten Frame zurueck."""
+        self.frame, self.face = self.detect.detectFace(frame)
+        if self.face is not None:
+            if save_face:
+                # Training-Set erstellung
+                self.trigger_save = True
+                self.detect.acceptNewFace(self.face, face_id)
+            elif self.trigger_save: # Nur einmal nach Beenden der Training-Set Aufnahme
+                self.trigger_save = False
+                log.info('Habe Training-Set beendet!')
+                # TODO: Lernen der neu aufgenommenen Bilder hier starten
+            elif recognize_face:                
+                self.trigger_rec = True
+                # Facedetection
+                predicted = self.fr.predict(self.face)
+                print 'it predicts: %s' % predicted
+                if predicted > 0:
+                    self.id_dict[str(predicted)][1] += 1
+                    
+            elif self.trigger_rec: # nur einmal bei Beenden der Gesichtserkennung
+                log.info('Beende Gesichtserkennung...')
+                # TODO: sortierung checken, gibt noch nicht korrekte ERG wieder
+                self.final_predict = 500 #sorted(self.id_dict.items(), key=lambda (k,v): v[1])[0][0]
+                log.debug('FINAL predict %s', self.final_predict)
+                self.notify_observer()                
+                # Aufraeumen
+                self.trigger_rec = False
+                self.print_stat()
+                self.final_predict = None
+                # Leeren der gemerkten predicts, damit bei nochmaligem Start die liste Leer ist
+                for k, v in self.id_dict.items():
+                    v[1] = 0
+        return self.frame
+    
---
+++ pascal/beta/gui.py
@@ -0,0 +1,182 @@
+# -*- coding: utf-8 -*-
+"""
+PyQt-GUI-Modul, Benutzeroberflaeche der gesamten Anwendung sowie noetige
+Video-Bild-Konvertierungen für PyQt Support.
+ 
+"""
+import logging as log
+
+from PyQt4 import Qt, QtCore, QtGui
+import numpy as np
+import cv2
+
+import controller as c
+
+class Video():
+    """Klasse zum konvertieren des Videobilds"""
+    def __init__(self, webcam, face_id=None, save_face=False, recognize_face=False, recognize_face_stopped=False):
+        self.webcam = webcam
+        self.face_id = face_id
+        self.save_face = save_face
+        self.recognize_face = recognize_face
+        self.stop = recognize_face_stopped
+        self.current_frame=np.ndarray([])
+        self.controller = c.Controller()
+        self.controller.register_observer(self)
+        self.observer = []
+    
+    def register_observer(self, obj):
+        """Wird zum registrieren eines Observers verwendet"""
+        self.observer.append(obj)
+        
+    def notify_observer(self, predict):
+        """Ruft update() aller zu benachritigen Objekte auf"""
+        for obj in self.observer:
+            assert(obj.update)
+            obj.update(predict)
+            
+    def update(self):
+        """Wird vom Observierten Objekt aufgerufen wenn es sich geandert hat"""
+        log.debug('update() von Video!!')
+        self.notify_observer(self.controller.get_final_predict())
+        
+    def capture_next_frame(self):
+        """Liest naechsten Frame der Kamera und wandelt es von BGR zu RGB und startet die Gesichtserkennung"""
+        success, read_frame=self.webcam.read()
+        if success:              
+            read_frame = cv2.cvtColor(read_frame, cv2.COLOR_BGR2RGB)
+            self.current_frame = self.controller.frame_to_face(read_frame,self.face_id,self.save_face, self.recognize_face)            
+    
+    def convert_frame(self):
+        """Konvertiert Bild in ein von QtGUI akzeptiertes Format"""
+        try:            
+            height, width = self.current_frame.shape[:2]
+            image = QtGui.QImage(self.current_frame, width, height,
+                                 QtGui.QImage.Format_RGB888)
+            image = QtGui.QPixmap.fromImage(image)
+            #self.previous_frame = self.current_frame
+            return image
+        except:
+            log.exception("Fehler beim konvertieren des Kamerabildes")
+            raise
+        
+class GUI(QtGui.QMainWindow):
+    """PyQt GUI fuer Button-Support und effiziente Kameraansteuerung."""
+    def __init__(self, *args):
+        """Buttons und ein Label fuer das Videobild sowie ein Timer zum 
+        periodischen Ausfuehren der play() Methode
+        
+        """
+        # Hauptlayout Vertikal-Boxlayout
+        QtGui.QWidget.__init__(self, *args)
+        # selbst als Vater und Hauptwidget setzen 
+        widget = QtGui.QWidget(self)
+        self.setCentralWidget(widget)
+        # vertikales Layout setzen
+        v_box_layout = QtGui.QVBoxLayout()    
+        widget.setLayout(v_box_layout)
+        
+        # QLabel als Videoframe Container
+        self.video_label = QtGui.QLabel("Videobild")
+        v_box_layout.addWidget(self.video_label)
+        
+        # Bedienelemente
+        palette = QtGui.QPalette()
+        # Wer-Bin-Ich-Button
+        self.button_who_i_am = QtGui.QPushButton("Wer-Bin-Ich?", self)
+        self.button_who_i_am.setCheckable(True)
+        palette.setColor(self.button_who_i_am.foregroundRole(), Qt.QColor("green"))
+#         self.font_bold = QtGui.QFont("Arial", 55, QtGui.QFont.Bold)
+#         self.button_who_i_am.fontChange(self.font_bold)
+        self.button_who_i_am.setPalette(palette)
+        Qt.QObject.connect(self.button_who_i_am, Qt.SIGNAL('clicked()'), self.clicked_who_i_am)
+        self.button_who_i_am.setMinimumHeight(60)
+        v_box_layout.addWidget(self.button_who_i_am)
+        # SubLayout fuer Textfelder nebeneinander
+        h_line_layout_text = QtGui.QHBoxLayout()
+        v_box_layout.addLayout(h_line_layout_text)
+        # Namensfeld Textfeld
+        self.text_name = QtGui.QLineEdit("Name", self)
+        Qt.QObject.connect(self.text_name, Qt.SIGNAL('textChanged(const QString&)'), self.on_input_name)
+        h_line_layout_text.addWidget(self.text_name)
+        # ID Textfeld
+        self.text_id = QtGui.QLineEdit("ID", self)
+        Qt.QObject.connect(self.text_id, Qt.SIGNAL('textChanged(const QString&)'), self.on_input_id)
+        h_line_layout_text.addWidget(self.text_id)
+        # Training-Set-Aufnehmen-Button
+        self.button_do_train = QtGui.QPushButton("Training-Set", self)
+        self.button_do_train.setCheckable(True)
+        v_box_layout.addWidget(self.button_do_train)
+        Qt.QObject.connect(self.button_do_train, Qt.SIGNAL('clicked()'), self.clicked_do_train)
+        # Beenden-Button
+        self.button_quit = QtGui.QPushButton("Ende", self)
+        v_box_layout.addWidget(self.button_quit)
+        palette.setColor(self.button_quit.foregroundRole(),Qt.QColor("red"))
+        self.button_quit.setPalette(palette)
+        Qt.QObject.connect(self.button_quit, Qt.SIGNAL('clicked()'), Qt.qApp,
+                           Qt.SLOT('quit()'))
+        v_box_layout.addWidget(self.button_quit)
+
+        # Video-Bild umwandeln und updaten
+        self.webcam = cv2.VideoCapture(0)
+        if self.webcam.isOpened(): 
+            self.video = Video(self.webcam)
+            self.video.register_observer(self)
+            self._timer = QtCore.QTimer(self)
+            # timeout-SIGNAL an play-SLOT binden
+            self._timer.timeout.connect(self.play)
+            self._timer.start(27)
+        else:
+            self.test = False
+            log.critical("Web-Cam nicht angeschlossen oder die Anwendung laeuft noch!?")
+    
+    def update(self, predict):
+        """Wird vom Observierten Objekt aufgerufen wenn es sich geandert hat"""
+        log.debug('update() von GUI!!')
+        self.text_id.setText(predict)
+        
+    def on_input_name(self, text):
+        """Wird automatisch bei Eingabe ins Textfeld aufgerufen"""
+        pass
+    def on_input_id(self, text):
+        """Wird automatisch bei Eingabe ins Textfeld aufgerufen"""
+        pass
+        
+    def play(self):
+        """Zum stetig wiederholtem Aufrufen um Kamerabild zu aktualisieren"""
+        try:
+            # naechsten Frame holen und diesen konvertiert als Pixmap in GUI anzeigen
+            self.video.capture_next_frame()            
+            self.video_label.setPixmap(self.video.convert_frame())            
+        except Exception:
+            log.exception("GUI.play(): Kein Bild von Kamera oder Konvertierungsproblem beim Wandeln zum QPixmap!")
+    
+    # Button-Callback-Funktionen
+    def clicked_do_train(self):
+        log.info('Training-Set: %s', self.button_do_train.isChecked())
+        if self.button_who_i_am.isChecked():
+            self.button_do_train.setChecked(False)
+            log.error('Bitte erst Gesichtserkennung beenden!')
+            return
+        if self.button_do_train.isChecked():
+            self.button_do_train.setText("Anhalten")
+            self.video.save_face = True
+            self.video.face_id = self.text_id.text()
+        else: # not button.isChecked()
+            self.button_do_train.setText("Training-Set")
+            self.video.save_face = False
+        
+    def clicked_who_i_am(self):
+        log.info('wer bin ich geklickt: %s', self.button_who_i_am.isChecked())
+        if self.button_do_train.isChecked():                            
+            self.button_who_i_am.setChecked(False)
+            log.error('Bitte erst Trainings-Modus beenden!')
+            return
+        if self.button_who_i_am.isChecked():
+            self.button_who_i_am.setText("Anhalten")
+            self.video.recognize_face = True
+            self.video.face_id = self.text_id.text()
+        else: # not button.isChecked()
+            self.button_who_i_am.setText("Wer-Bin-Ich?")
+            self.video.recognize_face = False
+            self.video.stop = True
