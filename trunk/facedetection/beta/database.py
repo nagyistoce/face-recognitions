a56d7fa1e5b4836cdd000025313f34c7c0f8c630
pascal.trebert@gmail.com <pascal.trebert@gmail.com@f236faba-5df7-439f-39a2-45de644ad76b>
2014-02-05 00:27:03 +0000

    Exception: nur Numerische Ordnernamen sind gültige IDs, andere Ordnernamen werden nun mit einer kurzen Infomeldung übersprungen.

--- trunk/facedetection/beta/database.py
+++ trunk/facedetection/beta/database.py
@@ -3,7 +3,6 @@
 Modul zur Verwaltung der Persistenten Daten.
 Jedes Training-Set gehoert zu einer Person und der Ordnername entspricht der ID.
 
-
 """
 import os
 import errno
@@ -35,6 +34,10 @@ class TrainingSets(object):
         # TOD0: automatisch Ordnerstruktur anlegen falls sie noch nicht existiert
         self.init_folder_structure()
     
+#     def update_ids(self):
+#         """Aktualisiert das ids-Dictionary, damit mit aktuellen Daten gearbeitet wird"""
+#         
+        
     def get_image_name(self, face_id):
         """Gibt den Bildnamen fuer ein neu zu speicherndes Gesicht zurueck"""
         now = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S.%f')
@@ -45,14 +48,13 @@ class TrainingSets(object):
         # check ob Ordner bereits existiert
         path = os.path.join(path, str(face_id))
         if not os.path.exists(path):
-            print path , 'wird angelegt...'
+            log.info('%s wird angelegt...', path)
             try:
                 os.makedirs(path)
             except OSError, e:
                 if e.errno == errno.EEXIST:
-                    print 'ignoriere os.error'
+                    log.debug('ignoriere bekanntes os.error errno.EEXIST: %s', e)
     
-    # TODO: aufraeumen, einzeiler Funktion raus
     def init_folder_structure(self):
         """Legt Ordnerstrukur an"""
         self.create_folder(self.path)
@@ -61,19 +63,17 @@ class TrainingSets(object):
         """Fuegt ein Gesichtsbild dem entsprechenden Ordner (self.ID) hinzu"""     
         assert(isinstance(face, np.ndarray))
         folder = os.path.join(self.path, str(face_id))
-        
+        full_path = os.path.join(folder,self.get_image_name(face_id))
         if not os.path.exists(folder):
             self.create_folder(self.path, face_id)
         try:
-            cv2.imwrite(os.path.join(folder,self.get_image_name(face_id)), face)
-        except IOError as detail:
-            print "Fehler beim Abspeichern des Bildes", detail
-            
-            
+            cv2.imwrite(full_path, face)
+        except (IOError, Exception):
+            log.exception("Fehler beim Abspeichern des Bildes: %s", full_path)
                 
     def get_faces(self, id_path, face_images):
         """Liest alle Bilder einer bestimmten ID ein"""
-        id = id_path.split(os.sep)[-1]
+        face_id = id_path.split(os.sep)[-1]
         num_imgs = 0
         for img in os.listdir(id_path):
             try:
@@ -81,13 +81,13 @@ class TrainingSets(object):
                 im = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
                 face_images.append(np.asarray(im, dtype = np.uint8))
                 num_imgs +=1
-            except IOError,(errno,strerror):
-                print "I/O error{0}: {1}".format(errno, strerror)
+            except IOError as e:
+                log.exception("I/O error{0}: {1}".format(e.errno, e.strerror))
             except:
-                print "Unexpected error: ", sys.exc_info()[0]
+                log.exception("Nichterwarteter Fehler: %s", sys.exc_info()[0])
                 raise
-        log.info('ID: %s %s Bilder eingelesen', id, num_imgs)
-        self.ids[str(id)] = [num_imgs, []]
+        log.info('ID: %s %s Bilder eingelesen', face_id, num_imgs)
+        self.ids[str(face_id)] = [num_imgs, []]
         return face_images, num_imgs
             
     def get_all_faces(self):
@@ -95,10 +95,13 @@ class TrainingSets(object):
         face_images, face_ids = [], []
         for dirname, dirnames, filenames in os.walk(self.path):
             for subdirname in sorted(dirnames):
-                if subdirname != 'ID':
-                    id_path = os.path.join(dirname, subdirname)
+                try:
+                    face_id = int(subdirname)
+                    id_path = os.path.join(dirname, str(face_id))
                     face_images, number = self.get_faces(id_path, face_images)
-                    face_ids.extend([int(subdirname)] * number)
+                    face_ids.extend([face_id] * number)
+                except ValueError, e:
+                    log.info('Ueberspringe den Ordner %s da er keine gueltige ID darstellt.', subdirname)
         return face_images, face_ids
     
 if __name__ == '__main__':
